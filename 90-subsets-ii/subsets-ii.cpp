3823583091a59e165a468be0e6948bfe7246d1f2
komalverma04 <komal148btit21@igdtuw.ac.in>
2024-06-03 17:05:38 +0530

    Time: 6 ms (21.44%) | Memory: 15.8 MB (10.27%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    void solve(vector<int>& nums, set<vector<int>>& st, vector<int>& output, int i){
+        if(i == nums.size()){
+            sort(output.begin(), output.end());
+            st.insert(output);
+            return;
+        }
+        vector<int> output1 = output;
+        vector<int> output2 = output;
+        output1.push_back(nums[i]);
+        solve(nums, st, output1, i+1);
+        solve(nums, st, output2, i+1);
+        return;
+
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>> st;
+        vector<vector<int>> ans;
+        vector<int> output;
+        solve(nums, st, output, 0);
+        for(auto s: st){
+            ans.push_back(s);
+        }
+        return ans;
+    }
+};
