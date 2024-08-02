5107b400548319319434d0ed242b3cbfe39fb914
Komal Verma <komal148btit21@igdtuw.ac.in>
2024-08-02 23:31:11 +0530

    Time: 4 ms (56.03%) | Memory: 66.9 MB (8.51%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<int>st;
+        int diff = 0;
         int n = s.size(); string ans = "";
         for(int i = 0; i<n;i++){
             if(s[i] == '('){
-                st.push(i);
-            }else{
-                if(s[st.top()] == '('){
-                    if(st.size() == 1){
-                        int ind = st.top();
-                        ans = ans + s.substr(ind+1,i-ind-1);
-                    }
-                    st.pop();
+                if(diff > 0){
+                    ans = ans + s[i];
                 }
-            }
+                diff++;
+            }else{
+                diff--;
+                if(diff > 0){
+                    ans = ans + s[i];
+                }            }
         }
         return ans;
     }
