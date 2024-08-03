e85639a0f030399496df7c41cdff17b9acaf9b50
Komal Verma <komal148btit21@igdtuw.ac.in>
2024-08-04 02:23:13 +0530

    Time: 8 ms (63.57%) | Memory: 10 MB (7.74%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         unordered_map<char,int>mpp;
-        unordered_map<char,int>mpp2;
+       
         int n = s.size();
         int m = t.size();
         if(n != m)return false;
@@ -12,10 +12,10 @@ public:
         for(int i = 0; i<n;i++){
             if(mpp.find(t[i]) == mpp.end()){
                 return false;
-            }mpp2[t[i]]++;
+            }mpp[t[i]]--;
         }
         for(int i = 0; i<n;i++){
-            if(mpp[s[i]] != mpp2[s[i]]){
+            if(mpp[s[i]] != 0){
                 return false;
 
             }
